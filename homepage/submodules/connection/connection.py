ddcf2ce21a58830bdf8f86381e0d23544d766372
englisb <112978302+englisb@users.noreply.github.com>
2024-10-16 16:11:10 -0400

    file reorganization, added user data loading

--- homepage/submodules/connection/connection.py
+++ homepage/submodules/connection/connection.py
@@ -6,4 +6,4 @@ def check_connection():
 def display_connection_status(frame): 
     status = check_connection() 
     label = tk.Label(frame, text=f"Connection: {status}", fg="red" if status != "STABLE" else "green") 
-    label.pack(anchor="ne") 
+    label.pack(anchor="ne")
